75e02628a77c62137072ca6aff16d11af0910cdd
Tomchanio <34372563+Tomchanskiy@users.noreply.github.com>
2018-11-29 17:49:20 +0300

    Update CHttp.php

--- src/Transport/CHttp.php
+++ src/Transport/CHttp.php
@@ -8,32 +8,31 @@ class CHttp
     public function send($method, $params)
     {
         try {
-            if(!$response = $this->response($method, $params)) return false;
-            return json_decode((string) $response);
+            $response =json_decode($this->response($method, $params));
         } catch (\Exception $e) {
             throw new HttpException($e);
         }
-		return false;
+        return $response;
     }
-	protected function getHeaders() 
-	{
-		return [
-			'Content-Type: application/json',
-			'Authorization: apikey ' . config('centrifugo.apikey')
-		];
-	}
-	protected function response($method, $params) {
+    protected function getHeaders() 
+    {
+        return [
+            'Content-Type: application/json',
+            'Authorization: apikey ' . config('centrifugo.apikey')
+        ];
+    }
+    protected function response(string $method, array $params) {
         $ch = curl_init();
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_POST, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(['method' => $method, 'params' => (array)$params]));
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $this->getHeaders());
-		curl_setopt($ch, CURLOPT_URL, config('centrifugo.url'));
-		$data = curl_exec($ch);
-		$err = curl_errno($ch);
-		if($err) return false;
-		curl_close($ch);
-		return $data;
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(['method' => $method, 'params' => $params]));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->getHeaders());
+        curl_setopt($ch, CURLOPT_URL, config('centrifugo.url'));
+        $data = curl_exec($ch);
+        $err = curl_errno($ch);
+        if($err) return $err;
+        curl_close($ch);
+        return $data;
     }
 }
